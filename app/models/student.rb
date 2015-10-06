13332278bf59e84cfd5420e074454616fd9aca06
toti fanara <tfbkny@gmail.com>
2015-10-06 18:15:22 -0400

    merging w/ toti-css

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,15 +5,11 @@ class Student < ActiveRecord::Base
   has_many :student_skills
   has_many :skills, :through => :student_skills
 
-<<<<<<< HEAD
-
-
 
   def name
     "#{Contact.find(contact_id).first_name} #{Contact.find(contact_id).last_name}"
   end
-=======
+
   has_secure_password
->>>>>>> master
 
 end
