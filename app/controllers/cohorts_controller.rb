502eeaf3537aa3079ebd45657c304f1f6504ee42
Jaskaranjit Singh <singhjaskar1@gmail.com>
2015-10-06 16:27:16 -0400

    minor erors

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -82,7 +82,7 @@ class CohortsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cohort
-      
+      @cohort = Cohort.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
