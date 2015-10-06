6e673a2d3a140e038effa909e0ba1d1b3d66f2a8
toti fanara <tfbkny@gmail.com>
2015-10-06 17:36:05 -0400

    commit before pull;t

--- app/assets/javascripts/components/_contact_box.js.jsx
+++ app/assets/javascripts/components/_contact_box.js.jsx
@@ -7,9 +7,11 @@ var ContactBox = React.createClass({
     //data needs to be set equal to all values found. data also needs to be regenerated.
     data_array = this.props['contacts']['contacts']
     console.log(search_string)
-    if (data_array.forEach(function(element){element.first_name}) == search_string){
-      //test
-    }
+    // if (data_array.forEach(function(element){element.first_name}) == search_string){
+    // render the matching element
+    //   return {element}
+    // }
+
   },
 
   handleContactSearch: function(search_string) {
