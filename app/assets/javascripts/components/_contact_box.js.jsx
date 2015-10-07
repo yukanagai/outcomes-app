18949d846f5b635328312737a34b77f7e85f78d3
nialbima@gmail.com <nialbima@gmail.com>
2015-10-07 19:16:22 -0400

    end of day commit, FUCK. REACT

--- app/assets/javascripts/components/_contact_box.js.jsx
+++ app/assets/javascripts/components/_contact_box.js.jsx
@@ -1,37 +1,45 @@
 var ContactBox = React.createClass({
   getInitialState: function() {
-    return {data: this.props}
-    // return {data: this.props}
+    pull_contacts = this.props.contacts.contacts
+    return {data: pull_contacts}
   },
 
   loadContacts: function(search_string) {
     //data needs to be set equal to all values found. data also needs to be regenerated.
-    data_array = this.props['contacts']['contacts']
-    return_array = []
+    data_array = this.state
+    console.log(data_array)
+    return_array = this.props
+
+    //keeps our data intact
+    function capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
 
     search_array = search_string.split(' ')
-    // will only work for a full name. if there are three terms or a name with an extra space, will fail.
-    // also partial names don't work.
-    search_object = {first_name: search_array[0], last_name: search_array[1]}
+    search_array.forEach(function(word, i){
+      search_array[i] = capitalizeFirstLetter(word);
+    })
 
+    //this is our comparison object. if first_name and last_name don't
+    //get us what we need, we're being too anal
+    search_object = {first_name: search_array[0], last_name: search_array[search_string.length - 1]}
 
-    data_array.forEach(function(currentContact){
+    this.data.forEach(function(currentContact){
       if (currentContact['first_name'] == search_object['first_name'] ||
           currentContact['last_name'] == search_object['last_name']   ||
           currentContact['first_name'] == search_object['last_name']  ||
-          currentContact['last_name'] == search_object['first_name'] ){
+          currentContact['last_name'] == search_object['first_name']) {
         return_array.push(currentContact)
       }
     })
-    console.log(return_array)
-  },
+
+    this.setState({data: return_array})
+  }.bind(this),
 
   handleContactSearch: function(search_string) {
     //retrieves data
     this.loadContacts(search_string);
   },
-
-  // componentDidUpdate call
 
   render: function() {
     return (
--- app/assets/javascripts/components/_contact_list.js.jsx
+++ app/assets/javascripts/components/_contact_list.js.jsx
@@ -1,7 +1,7 @@
 var ContactList = React.createClass({
 
   render: function() {
-    var contactNodes = this.props.data.contacts.contacts.map(function(contact){
+    var contactNodes = this.props.data.map(function(contact){
         return(
           <tr>
             <Contact data={contact} key={contact.id}/>
