e15c605040b6266c9270fbeea654a70e6cb302b0
Jaskaranjit Singh <singhjaskar1@gmail.com>
2015-10-10 17:22:40 -0400

    email refactored

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,18 +1,10 @@
 class SurveysController < ApplicationController
 
-def index
-  #@user is a contact
-  @user = current_user
-
-  # redirect if user is officer
-  if @user.is_officer?
-    # add notice about not needing the survey
-    redirect_to dashboard_path
-  else
-    #set survey_user
+  def index
+    #@user is a contact
+    @user = current_user
     @survey_user = Student.find_by(contact: @user.id)
+    binding.pry
   end
-
-end
 
 end
