c5cfa2528059e145b71a326fa3373bddfc74af1d
nialbima@gmail.com <nialbima@gmail.com>
2015-10-11 08:52:55 -0400

    added a bunch of notes about how react works

--- app/assets/javascripts/components/contacts/_contact_box.js.jsx
+++ app/assets/javascripts/components/contacts/_contact_box.js.jsx
@@ -1,18 +1,23 @@
 var ContactBox = React.createClass({
+  // react_component is called on the view page, with "contacts: @contacts" as an argument.
+  // this.props.contacts.contacts here sets our initial state as "those contacts"
+  // by setting "data: data_array," it becomes available further down in the script
   getInitialState: function() {
     var data_array = this.props.contacts.contacts
     return {data: data_array}
   },
 
+  //componentDidMount checks whether something loaded. right now, i just have it logging the program's state.
   componentDidMount: function() {
     console.log(this.state)
   },
 
+  //modifies the state based on search_string. i pass in search_string in a different component.
   loadContacts: function(search_string) {
     //data needs to be set equal to all values found. data also needs to be regenerated.
     return_array = []
 
-    //keeps our data intact
+    //string cleaning
     function capitalizeFirstLetter(string) {
       return string.charAt(0).toUpperCase() + string.slice(1);
     }
@@ -22,10 +27,10 @@ var ContactBox = React.createClass({
       search_array[i] = capitalizeFirstLetter(word);
     })
 
-    //this is our comparison object. if first_name and last_name don't
-    //get us what we need, we're being too anal
+    //this is our comparison object. we set it up, and then compare.
     var search_object = {first_name: search_array[0], last_name: search_array[search_string.length - 1]}
 
+    //searches props for a matching name.
     this.props.contacts.contacts.forEach(function(currentContact){
       if (currentContact['first_name'] == search_object['first_name'] ||
           currentContact['last_name'] == search_object['last_name']   ||
@@ -39,11 +44,17 @@ var ContactBox = React.createClass({
     this.setState({data: return_array})
   },
 
+  //we need an event to trigger loadContacts.
   handleContactSearch: function(search_string) {
 
     this.loadContacts(search_string);
   },
 
+  //and finally, the render call. render kicks off everything in react.
+  //the params passed into ContactList are important.
+  //Setting onContactSearch={this.handleContactSearch} makes onContactSearch available in ContactList,
+  //and invokes the correct method here.
+  //data set to state updates the field based on changes in the state.
   render: function() {
     return (
       <div className="contact-box">
--- app/assets/javascripts/components/contacts/_contact_search_form.js.jsx
+++ app/assets/javascripts/components/contacts/_contact_search_form.js.jsx
@@ -1,14 +1,18 @@
 var ContactSearchForm = React.createClass({
   handleSubmit: function(event) {
+    //in rails, this keeps a submit button from passing a post request to the controller.
     event.preventDefault()
+    //we gave our text-input field a ref of "search_string", so we could grab it off of the DOM and find it's value.
     var search_string = React.findDOMNode(this.refs.search_string).value;
     //if not there, do nothing
     if (!search_string) {
       return;
     }
 
+    //refers back up to the onContactSearch prop, and passes search_string   up the chain
     this.props.onContactSearch(search_string);
 
+    //resets the value
     React.findDOMNode(this.refs.search_string).value="";
     return;
   },
@@ -16,6 +20,8 @@ var ContactSearchForm = React.createClass({
   render: function() {
     return (
       <form className="search-form" onSubmit={this.handleSubmit}>
+        //the ref is important here. it keeps track of what we're calling.
+        //adding onSubmit invokes the this.handleSubmit method.
         <input type="text" placeholder="Enter name!" ref="search_string"/>
         <input type="submit" value="Search"/>
       </form>
